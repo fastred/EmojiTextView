7edda50c45e945a980ddf8e1066b991785684025
Arkadiusz Holko <fastred@fastred.org>
2016-07-17 00:49:58 +0200

    Add Xcode project with empty framework and example

---
+++ EmojiTextView/EmojiTextView.h
@@ -0,0 +1,19 @@
+//
+//  EmojiTextView.h
+//  EmojiTextView
+//
+//  Created by Arkadiusz Holko on 17/07/16.
+//  Copyright © 2016 Arkadiusz Holko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EmojiTextView.
+FOUNDATION_EXPORT double EmojiTextViewVersionNumber;
+
+//! Project version string for EmojiTextView.
+FOUNDATION_EXPORT const unsigned char EmojiTextViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmojiTextView/PublicHeader.h>
+
+
---
+++ Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Arkadiusz Holko on 17/07/16.
+//  Copyright © 2016 Arkadiusz Holko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
